ad30ebd6238529a27944475bc02e3bc87e4b5d09
Prince-nkrumah <nkrprince69@gmail.com>
2025-07-29 19:48:28 +0000

    added url

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ sequelize.authenticate()
 // ✅ Middleware: Static files (e.g., images in /public)
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+app.set('trust proxy', 1);
+
 // ✅ CORS setup
 app.use(cors({
   origin: [
