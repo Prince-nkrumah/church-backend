f7c5ac6e1c8bab25244b86797ac209da9a8e05a6
Prince-nkrumah <nkrprince69@gmail.com>
2025-07-29 19:43:41 +0000

    added url

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(cors({
   origin: [
     'http://127.0.0.1:5500', 
-    'http://localhost:5500'
+    'http://localhost:5500',
     'https://cosmicchristglories.vercel.app'
   ],
   credentials: true
